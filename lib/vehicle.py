7e2c8bfe0900bfdc09909b458158c34c1e399109
ubah feisal <feisalubah1@gmail.com>
2023-09-11 16:59:56 +0300

    done

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,14 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number): #initializing method
+        self.wheel_number = wheel_number
+        self.wheel_size = wheel_size
+        
+    def go(self): #instance method
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self): #instance method
+        return "filling up!"
+    
+toyota = Vehicle(20,10) #object Toyota
+print(toyota.go())
+print(toyota.fill_up_tank())
